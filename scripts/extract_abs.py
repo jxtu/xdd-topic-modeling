0f21c36fd494b049a4473f21b9b5b68d0eaf9d72
jxtu <jxtu@brandeis.edu>
2022-09-30 11:28:43 -0400

    Added scripts for getting abstracts.

--- scripts/extract_abs.py
+++ scripts/extract_abs.py
@@ -1,5 +1,8 @@
+import json
 from pathlib import Path
 import re
+
+import click
 import tqdm
 
 
@@ -16,16 +19,26 @@ def extract_abs_from_txt(text: str):
     return ""
 
 
-if __name__ == '__main__':
-    full_text_dir = "../xdd-covid-19-8Dec-doc2vec_text"
+@click.command()
+@click.argument("text_dir", type=click.Path(exists=True), default="xdd-covid-19-8Dec-doc2vec_text")
+@click.argument("out_json_file", type=click.Path(), default="xdd-covid-19-abs.json")
+def abs_to_json(text_dir, out_json_file) -> None:
     count_all = 0
     count_has_abs = 0
-    for p in tqdm.tqdm(Path(full_text_dir).iterdir()):
-        # print(p)
-        count_all += 1
-        text = open(p, "r").read()
-        abs_txt = extract_abs_from_txt(text)
-        if abs_txt:
-            count_has_abs += 1
-        # print("====" * 10)
-    print(count_all, count_has_abs, count_has_abs / count_all)
+    abs_dict = dict()
+    for p in tqdm.tqdm(Path(text_dir).iterdir()):
+        if p.suffix == ".txt":
+            gdd_id = p.stem
+            count_all += 1
+            full_text = open(p, "r").read()
+            abs_txt = extract_abs_from_txt(full_text)
+            if abs_txt:
+                abs_dict[gdd_id] = abs_txt
+                count_has_abs += 1
+    with open(out_json_file, "w") as f:
+        f.write(json.dumps(abs_dict, indent=4))
+    print(f"extracted {count_has_abs} abstracts from {count_all} articles")
+
+
+if __name__ == '__main__':
+    abs_to_json()
---
+++ scripts/split_on_abs.py
@@ -0,0 +1,36 @@
+import json
+import numpy as np
+from pathlib import Path
+
+DATA_DIR = Path("xdd-covid-19-8Dec-doc2vec")
+json_file_name = "xdd-covid-19-8Dec.bibjson"
+np_d2v_file_name = "model_streamed_doc2vec.docvecs.vectors_docs.npy"
+abs_json_file = "xdd-covid-19-abs.json"
+
+
+def sample_article_with_abs(in_json_file, in_np_file, in_abs_file) -> None:
+    json_objs = json.load(open(DATA_DIR.joinpath(in_json_file), "r", encoding="utf-8"))
+    json_abs = json.load(open(DATA_DIR.joinpath(in_abs_file), "r", encoding="utf-8"))
+    np_objs = np.load(str(DATA_DIR.joinpath(in_np_file)))
+
+    sampled_json_objs = []
+    sampled_np_objs = []
+
+    assert len(json_objs) == np_objs.shape[0], "size do not match!"
+    for i, json_obj in enumerate(json_objs):
+        ggd_id = json_obj["_gddid"]
+        if json_abs.get(ggd_id):
+            json_obj["abs"] = json_abs.get(ggd_id)
+            sampled_json_objs.append(json_obj)
+            sampled_np_objs.append(np_objs[i])
+
+    with open(DATA_DIR.joinpath(f"abs_{in_json_file}"), "w", encoding="utf-8") as f:
+        json.dump(sampled_json_objs, f, indent=4)
+    np.save(str(DATA_DIR.joinpath(f"abs_{in_np_file}")), np.array(sampled_np_objs))
+
+    print(f"finished dumping instances with abstract!")
+
+
+if __name__ == '__main__':
+    sample_article_with_abs(json_file_name, np_d2v_file_name, abs_json_file)
+
